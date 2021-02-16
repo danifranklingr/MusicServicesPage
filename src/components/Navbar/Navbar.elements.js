ec1c075f058e4496adb66cfdf48513c139877e07
Dani Franklin <danifranklingr@gmail.com>
2021-02-16 00:42:09 -0300

    Se añadió el Icono de Menú a la Derecha de Navbar

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -35,7 +35,20 @@ export const NavLogo = styled(Link)`	// Para construir NavLogo se heredan las pr
 	align-items: center;
 `;
 
-export const NavIcon = styled(FaMagento)`
+export const NavIcon = styled(FaMagento)`		// Un React Icon Modificado en tamaño.
 	margin-right: 0.5rem;
+`;
+
+export const MobileIcon = styled.div`			// Este es el contenedor del icono de menú a la derecha de la barra.
+	display: none;										
+	@media screen and (max-width: 960px){			// Media query para que el icono solo aparezca para pantallas 
+		display: block;									// más pequeñas.
+		position: absolute;
+		top: 0;
+		right: 0;
+		transform: translate(-100%,60%);
+		font-size: 1.8rem;
+		cursor: pointer;
+	}
 `;
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import {Nav, NavbarContainer, NavLogo, NavIcon} from "./Navbar.elements";
+import React,{useState} from 'react';
+import {Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon} from "./Navbar.elements";
+import {FaTimes, FaBars} from "react-icons/fa";
+import {IconContext} from "react-icons/lib";
 
 const Navbar = () => {
+	const [Click, setClick] = useState(false);
+
+	const HandleClick = () => setClick(!Click);
+
 	return (
-		<Nav>
-			<NavbarContainer>
-				<NavLogo to="/">
-					<NavIcon/>
-					DANI
-				</NavLogo>
-			</NavbarContainer>
-		</Nav>
+		<IconContext.Provider value={{color:"wheat"}}>
+			<Nav>
+				<NavbarContainer>
+					<NavLogo to="/">
+						<NavIcon/>
+						DANI
+					</NavLogo>
+					<MobileIcon	onClick={HandleClick}>
+						{Click ? <FaTimes/> : <FaBars/>}
+					</MobileIcon>
+				</NavbarContainer>
+			</Nav>
+		</IconContext.Provider>
 	)
 }
 
