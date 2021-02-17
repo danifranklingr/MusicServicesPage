7f7c27dc0a28d24e516c26e440dc1a66275ac5c6
Dani Franklin <danifranklingr@gmail.com>
2021-02-17 00:15:15 -0300

    Comentarios Corregidos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {Navbar} from "./components";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import GlobalStyle from "./globalStyles";
 
-//				Minuto 49
+//				Minuto 1:02:00
 // https://www.youtube.com/watch?v=iP_HqoCuRI0&list=PLrai-is-V9ea3sRtdPI4VuRgG_QqOH4G9&index=15&t=1805s
 
 
